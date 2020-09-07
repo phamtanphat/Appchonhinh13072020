ca62ff732efdec3f3d9f7387026763f1d9c72eeb
phatdroid94 <phatdroid94@gmail.com>
2020-09-07 19:25:18 +0700

    navigate to history and random

--- app/src/main/java/com/example/appchonhinh13072020/HomeActivity.java
+++ app/src/main/java/com/example/appchonhinh13072020/HomeActivity.java
@@ -2,13 +2,34 @@ package com.example.appchonhinh13072020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.appchonhinh13072020.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+       binding.btnNewGame.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(HomeActivity.this,RandomActivity.class);
+               startActivity(intent);
+           }
+       });
+       binding.btnHistory.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(HomeActivity.this,HistoryActivity.class);
+               startActivity(intent);
+           }
+       });
     }
 }
---
+++ app/src/main/java/com/example/appchonhinh13072020/RandomActivity.java
@@ -0,0 +1,14 @@
+package com.example.appchonhinh13072020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RandomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random);
+    }
+}
