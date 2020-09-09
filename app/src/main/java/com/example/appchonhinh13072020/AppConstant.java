c3848f200e264f8e6b4a9c0a3d5dc7343e2879d9
phatdroid94 <phatdroid94@gmail.com>
2020-09-09 18:06:57 +0700

    app constant

---
+++ app/src/main/java/com/example/appchonhinh13072020/AppConstant.java
@@ -0,0 +1,5 @@
+package com.example.appchonhinh13072020;
+
+public class AppConstant {
+    public static final Integer TOTAL_TIME = 3000;
+}
--- app/src/main/java/com/example/appchonhinh13072020/RandomActivity.java
+++ app/src/main/java/com/example/appchonhinh13072020/RandomActivity.java
@@ -3,12 +3,43 @@ package com.example.appchonhinh13072020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import com.example.appchonhinh13072020.databinding.ActivityRandomBinding;
 
 public class RandomActivity extends AppCompatActivity {
 
+    ActivityRandomBinding mBinding;
+    CountDownTimer mTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_random);
+        mBinding = ActivityRandomBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        init();
+        startTime();
+        // Task 1 : Thoi gian choi game
+        // Task 2 : Random hinh anh
+        // Task 3 : Chon hinh
+    }
+
+    private void init() {
+    }
+
+    private void startTime() {
+        if (mTimer != null){
+            mTimer.cancel();
+            mTimer = new CountDownTimer() {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                }
+            };
+        }
     }
 }
